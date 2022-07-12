80866ee7e680991206b4fc0fbc051d55e07b8270
Raghu Ganapathy <raghu.1999@gmail.com>
2022-07-13 04:02:57 +0530

    updated favicon

--- pages/_document.js
+++ pages/_document.js
@@ -1,19 +1,39 @@
-import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
-import GoogleFonts from 'next-google-fonts'
-import { ColorModeScript } from "@chakra-ui/react"
+import NextDocument, { Html, Head, Main, NextScript } from "next/document";
+import GoogleFonts from "next-google-fonts";
+import { ColorModeScript } from "@chakra-ui/react";
 
 export default class MyDocument extends NextDocument {
-    render() {
-        return (
-            <Html lang="en">
-                {/* <GoogleFonts href={`https://fonts.googleapis.com/css2?family=Inter:wght@400;600;800&display=swap`} /> */}
-                <Head />
-                <body>
-                    <ColorModeScript />
-                    <Main />
-                    <NextScript />
-                </body>
-            </Html>
-        )
-    }
+  render() {
+    return (
+      <Html lang="en">
+        {/* <GoogleFonts href={`https://fonts.googleapis.com/css2?family=Inter:wght@400;600;800&display=swap`} /> */}
+        <Head>
+          <link rel="shortcut icon" href="/favicon.ico" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest"></link>
+        </Head>
+        <body>
+          <ColorModeScript />
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
